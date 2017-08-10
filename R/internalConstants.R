91f302b9887aa5c4c6cfd3e1ad031b77479e4b19
MrDAndersen <andersen.dennis@live.com>
2017-08-09 21:42:51 -0500

    Adding Rob Kelley name correction

--- R/internalConstants.R
+++ R/internalConstants.R
@@ -13,12 +13,12 @@ correct.from <- c("Ty Hilton", "Timothy Wright", "Christopher Ivory", "Domanique
                   "Corey Philly Brown",  "Foswhitt Whittaker", "CJ Anderson", "TY Hilton", "Boobie Dixon", "EZ Nwachukwu", "Dave Paulson",
                   "Joe DonDuncan", "T Y Hilton", "Dqwell Jackson", "Art Jones", "Navorro Bowman", "Devante Parker", "A.J. McCarron", "TJ Yeldon",
                   "CJ Prosise", "AJ Green", "David A. Johnson", "Adrian L. Peterson", "Jonathan C. Stewart", "Chris D. Johnson", "Austin D. Johnson",
-                  "Steve L. Smith", "Michael A. Thomas", "Devin A. Smith", "Michael D. Thomas")
+                  "Steve L. Smith", "Michael A. Thomas", "Devin A. Smith", "Michael D. Thomas", "Robert Kelley")
 
 correct.to <- c("T.Y. Hilton", "Tim Wright", "Chris Ivory", "Dominique Davis", "Benjamin Watson", "Steve Johnson", "LeSean McCoy",
                 "Luke Willson",	"Thad Lewis", "Walt Powell", "Wilson Van Hooser", "Steven Hauschka", "Dan Herron", "Rob Housler",
                 "Philly Brown", "Fozzy Whittaker", "C.J. Anderson", "T.Y. Hilton", 	"Anthony Dixon", "Uzoma Nwachukwu", "David Paulson",
                 "Joe Don Duncan",	"T.Y. Hilton", "DQwell Jackson", "Arthur Jones", "NaVorro Bowman", "DeVante Parker", "AJ McCarron",
                 "T.J. Yeldon", "C.J. Prosise", "A.J. Green", "David Johnson", "Adrian Peterson" ,"Jonathan Stewart", "Chris Johnson", "Austin Johnson",
-                "Steve Smith", "Michael Thomas", "Devin Smith", "Michael Thomas")
+                "Steve Smith", "Michael Thomas", "Devin Smith", "Michael Thomas", "Rob Kelley")
 
